0bf4c5ae21113cefb93233a3ce01b83f69e4e29b
Axel Moncayo <axel.moncayo.dev@gmail.com>
2024-01-01 12:13:09 -0600

    A full viewport in every section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        //prettier-ignore
+        'widescreen': { raw: "(min-aspect-ratio: 3/2)" },
+        //prettier-ignore
+        'tallscreen': { raw: "(min-aspect-ratio: 1/2)" },
+      },
+    },
   },
   plugins: [],
 };
